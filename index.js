c2aec0d0ba6aefef9ba759cabd504a0cc1a5b34c
kylegavinsimone <kylesimone@outlook.com>
2021-03-24 06:36:59 -0700

    index update

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
-
+const jest=require("jest")
 const Engineer = require("./lib/Engineer.js");
 const Manager = require("./lib/Manager.js");
 const Intern = require("./lib/Intern.js");
-
+const Employee = require("./lib/Employee.js");
 const generateHTML = require("./generateHTML");
-const writeHTML = require("./generateHTML");
-
-const teamAnswers = [];
+const writeHTML = require("./app.js");
+let teamAnswers = [];
+console.log(teamAnswers);
 
 function userInput() {
   return inquirer
     .prompt([
+      {
+        type: "input",
+        message: "Employee's Name?",
+        name: "name",
+        validate: nameInput =>{
+            if(nameInput){
+                return true;
+            }else {
+                console.log("Please enter an employee's name. ")
+                return false;
+            }
+        }
+      },
+      {
+        type: "input",
+        message: "Employee's ID?",
+        name: "id",
+        validate: idInput =>{
+            if(idInput){
+                return true;
+            }else {
+                console.log("Please enter a number ID. ")
+                return false;
+            }
+        }
+      },
+      {
+        type: "input",
+        message: "Employee's email address?",
+        name: "email",
+        validate: emailInput =>{
+            if(emailInput){
+                return true;
+            }else {
+                console.log("Please enter a valid @email address. ")
+                return false;
+            }
+        }
+      },
       {
         type: "list",
         message: "What is the employee's role?",
@@ -28,34 +67,32 @@ function userInput() {
           .prompt([
             {
               type: "input",
-              message: "Engineer's Name?",
-              name: "name",
-            },
-            {
-              type: "input",
-              message: "Engineer's ID?",
-              name: "id",
-            },
-            {
-              type: "input",
-              message: "Engineer's email address?",
-              name: "email",
-            },
-            {
-              type: "input",
               message: "What is the Engineer's Github username?",
               name: "github",
+              validate: githubInput =>{
+                if(githubInput){
+                    return true;
+                }else {
+                    console.log("Please enter a Github username. ")
+                    return false;
+                }
+            }
             },
           ])
-          .then(function (engineerAns) {
+          .then((ans) => {
+            console.log(ans.github);
+            console.log(answers.role);
+
             const engineer = new Engineer(
-              engineerAns.name,
-              engineerAns.id,
-              engineerAns.email,
-              engineerAns.role,
-              engineerAns.github
-            );
+              answers.name,
+              answers.id,
+              answers.email,
+              answers.role,
+              ans.github
+            )
+
             console.log(engineer);
+
             teamAnswers.push(engineer);
             addEmployee();
           });
@@ -64,33 +101,28 @@ function userInput() {
           .prompt([
             {
               type: "input",
-              message: "Manager's Name?",
-              name: "name",
-            },
-            {
-              type: "input",
-              message: "Manager's ID?",
-              name: "id",
-            },
-            {
-              type: "input",
-              message: "Manager's email address?",
-              name: "email",
-            },
-            {
-              type: "input",
               message: "What is the Manager's office number?",
               name: "officeNumber",
+              validate: officeNumberInput =>{
+                if(officeNumberInput){
+                    return true;
+                }else {
+                    console.log("Please enter an Office Number. ")
+                    return false;
+                }
+            }
             },
           ])
-          .then(function (managerAns) {
+          .then((ans) => {
+            console.log(ans.officeNumber);
+
             const manager = new Manager(
-              managerAns.name,
-              managerAns.id,
-              managerAns.email,
-              managerAns.role,
-              managerAns.officeNumber
-            );
+              answers.name,
+              answers.id,
+              answers.email,
+              answers.role,
+              ans.officeNumber
+            )
             console.log(manager);
             teamAnswers.push(manager);
             addEmployee();
@@ -100,37 +132,41 @@ function userInput() {
           .prompt([
             {
               type: "input",
-              message: "Intern's Name?",
-              name: "name",
-            },
-            {
-              type: "input",
-              message: "Intern's ID?",
-              name: "id",
-            },
-            {
-              type: "input",
-              message: "Intern's email address?",
-              name: "email",
-            },
-            {
-              type: "input",
               message: "What school is the intern attending?",
               name: "school",
+              validate: schoolInput =>{
+                if(schoolInput){
+                    return true;
+                }else {
+                    console.log("Please enter a school name. ")
+                    return false;
+                }
+            }
             },
           ])
-          .then(function (internAns) {
+          .then((ans) => {
+            console.log(ans.school);
+
             const intern = new Intern(
-              internAns.name,
-              internAns.id,
-              internAns.email,
-              internAns.role,
-              internAns.school
-            );
+              answers.name,
+              answers.id,
+              answers.email,
+              answers.role,
+              ans.school
+            )
             console.log(intern);
             teamAnswers.push(intern);
             addEmployee();
           });
+      } else {
+        const employee = new Employee(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.role
+        )
+        teamAnswers.push(employee);
+        addEmployee();
       }
     })
     .catch(function (err) {
@@ -146,80 +182,16 @@ function addEmployee() {
         name: "next",
       },
     ])
-    .then(function (confirmAnswer) {
-      confirmAnswer.next ? userInput() : null;
-    })
-    // .then(answers=>{
-    //     if(answers.next === true){
-    //         userInput(teamAnswers)
-    //     }else{
-    //         null
-    //         console.log("teamAnswers", teamAnswers)
-    //         let cardsHTML = generateHTML(teamAnswers)
-    //         writeHTML(cardsHTML)
-    //     }
-    .catch(function (err) {
-      console.log(err);
-    })
 
-    // .catch(function(err){
-    //     console.log(err);
-    // })
-
-    .then((answers) => {
-      console.log(answers);
-      const htmlPageContent = generateHTML(teamAnswers);
-      console.log(htmlPageContent);
-      fs.writeFile("sampleIndex.html", htmlPageContent, (err) =>
-        err ? console.log(err) : console.log("Successfully created index.html!")
-      );
+    .then((ans) => {
+      if (ans.next === true) {
+        userInput(teamAnswers);
+      } else {
+        console.log("teamAnswers", teamAnswers);
+        let cardsHTML = generateHTML(teamAnswers);
+        writeHTML(cardsHTML);
+      }
     });
 }
-// function writeSite(html){
-//     fs.writeFile("sampleIndex.html", html, err =>{
-//         if(err){
-//             throw err
-//         }
-//         console.log("saved")
-//         fs.copyFile()
-//     })
-// }
-// const copyFile=()=>{
-//     fs.copyFile("./dist/style.css", err =>{
-//         if(err){
-//             console.log(err);
-//         }
-//         console.log("css copied")
-//     })
-// }
-// }
 
 userInput();
-
-// <div class="card bg-primary" style="width: 18rem;">
-//         <div class="card-header">
-//           <h5 class="managerName card-title text-white">${data.managerName}</h5>
-//           <h6 class="managerRole card-subtitle mb-2 text-white">${data.role}</h6>
-//         </div>
-//         <div class="card-body">
-//           <ul class="list-group list-group-flush">
-//             <li class="managerId list-group-item">ID: ${data.managerId}</li>
-//             <li class="managerEmail list-group-item">Email: ${data.managerEmail}</li>
-//             <li class="officeNumber list-group-item">Office Number: ${data.officeNumber}</li>
-//           </ul>
-//         </div>
-//       </div>
-
-//       <div class="card bg-primary" style="width: 18rem;">
-//         <div class="card-header">
-//           <h5 class="internName card-title text-white">${data.internName}</h5>
-//           <h6 class="internRole card-subtitle mb-2 text-white">${data.role}</h6>
-//         </div>
-//         <div class="card-body">
-//           <ul class="list-group list-group-flush">
-//             <li class="internId list-group-item">ID: ${data.internId}</li>
-//             <li class="internEmail list-group-item">Email: ${data.internEmail}</li>
-//             <li class="school list-group-item">School: ${data.school}</li>
-//           </ul>
-//         </div>
-//       </div>
